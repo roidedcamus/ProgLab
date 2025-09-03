e2b58178bc134f14033b066bf59ddcc0720c1ea2
roidecamus <akgnerf@gmail.com>
2025-09-03 23:43:12 +0530

    Mario done

---
+++ personalprac/mario2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Height: ");
+    scanf("%d", &n);
+
+    for(int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < n - i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int p = 0; p < n - i + 1; p++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }    
+}
